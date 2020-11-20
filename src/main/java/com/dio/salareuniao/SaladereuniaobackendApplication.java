3a8e8f6828f16e37d2d0533c8f922396b69e5538
unknown <jaco-miranda@hotmail.com>
2020-11-20 09:39:14 -0400

    Iniciando pacotes do projeto

---
+++ src/main/java/com/dio/salareuniao/SaladereuniaobackendApplication.java
@@ -0,0 +1,13 @@
+package com.dio.salareuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaladereuniaobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaladereuniaobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/salareuniao/SaladereuniaobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.salareuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladereuniaobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
